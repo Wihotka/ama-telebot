91898396c6014e442529f3d9d1be386e12ea6e8d
Pavel Finagin <60171573+Wihotka@users.noreply.github.com>
2023-01-20 18:40:43 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import styles from './App.module.scss';
+import styles from './scss/app.module.scss';
 
 function App() {
   return (
-    <div className={styles.app}>Эй Пидор!</div>
+    <div className={styles.app}>
+      <p>Учись, а то будешь как они:</p>
+      <img src="https://vk.com/id297037799?z=photo297037799_457247052%2Fphotos297037799" alt="freaks"/>
+    </div>
   );
 }
 
