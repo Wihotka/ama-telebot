06b3e2c0a6b8148df3f46471c2ab05b713a710f2
Pavel Finagin <60171573+Wihotka@users.noreply.github.com>
2023-01-24 15:22:41 +0300

    test

--- src/components/elements/botForm/BotForm.tsx
+++ src/components/elements/botForm/BotForm.tsx
@@ -29,11 +29,11 @@ export const BotForm = ({changeSentStatus, age, grade, subject, action}:P) => {
         tel: ''
       };
 
-      if (!values.name) errors.name = 'No name';
-      if (!values.surname) errors.surname = 'No surname';
-      if (!values.tel) errors.tel = 'No phone number';
-      else if (values.tel.length < 5) errors.tel = 'Too short phone number';
-      else if (!values.tel.match(/\d/i)) errors.tel = 'Incorrect phone number';
+      if (!values.name) errors.name = 'Error';
+      if (!values.surname) errors.surname = 'Error';
+      if (!values.tel) errors.tel = 'Error';
+      else if (values.tel.length < 5) errors.tel = 'Error';
+      else if (!values.tel.match(/\d/i)) errors.tel = 'Error';
 
       if (errors.name || errors.surname || errors.tel) {
         tg.MainButton.hide();
@@ -61,16 +61,19 @@ export const BotForm = ({changeSentStatus, age, grade, subject, action}:P) => {
   });
 
   const handleName = (e:{target:{value:any}}) => {
+    formik.touched.name = true;
     const value = upperFirst(e.target.value);
     e.target.value = value.trimStart().replace(/\s\s+/g, ' ');
     formik.handleChange(e);
   };
   const handleSurname = (e:{target:{value:any}}) => {
+    formik.touched.surname = true;
     const value = upperFirst(e.target.value);
     e.target.value = value.trimStart().replace(/\s\s+/g, ' ');
     formik.handleChange(e);
   };
   const handleTel = (e:{target:{value:any}}) => {
+    formik.touched.tel = true;
     const value = e.target.value;
     e.target.value = value.replace(/[^\d+\-() ]/g, '').replace(/\s\s+/g, ' ').substr(0, 20).trimStart();
     formik.handleChange(e);
@@ -80,6 +83,11 @@ export const BotForm = ({changeSentStatus, age, grade, subject, action}:P) => {
     tg.MainButton.setParams({
       text: 'Send data'
     });
+    tg.MainButton.hide();
+
+    formik.touched.name = false;
+    formik.touched.surname = false;
+    formik.touched.tel = false;
   }, []);
 
   return <form onSubmit={formik.handleSubmit} className={styles.form}>
@@ -89,33 +97,24 @@ export const BotForm = ({changeSentStatus, age, grade, subject, action}:P) => {
       placeholder='Name'
       name='name'
       onChange={handleName}
-      className={classNames(formik.errors.name && styles.inputError)}
+      className={classNames(formik.touched.name && formik.errors.name && styles.inputError)}
     />
-    {formik.touched.name && formik.errors.name
-      ? <div className={styles.error}>{formik.errors.name}</div>
-      : null}
     <BotInput
       type='text'
       value={formik.values.surname}
       placeholder='Surname'
       name='surname'
       onChange={handleSurname}
-      className={classNames(formik.errors.surname && styles.inputError)}
+      className={classNames(formik.touched.surname && formik.errors.surname && styles.inputError)}
     />
-    {formik.touched.surname && formik.errors.surname
-      ? <div className={styles.error}>{formik.errors.surname}</div>
-      : null}
     <BotInput
       type='tel'
       value={formik.values.tel}
       placeholder='Mobile phone'
       name='tel'
       onChange={handleTel}
-      className={classNames(formik.errors.tel && styles.inputError)}
+      className={classNames(formik.touched.tel && formik.errors.tel && styles.inputError)}
     />
-    {formik.touched.tel && formik.errors.tel
-      ? <div className={styles.error}>{formik.errors.tel}</div>
-      : null}
     {/* <button type='submit' className={styles.submitBtn}>Send data</button> */}
   </form>
 }
